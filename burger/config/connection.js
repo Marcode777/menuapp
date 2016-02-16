f1015c2aadfbe816b2a2ae2870d47a46dcdc795b
Marcode777 <marc102202@hotmail.com>
2016-02-16 17:46:22 -0500

    added  connection.js

---
+++ burger/config/connection.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var PORT = 3007;
+
+app.use(bodyParser.urlencoded({extended : false}));
+
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({defaultLayout:"main"}));
+app.set("view engine", "handlebars");
+
+var mysql = require("mysql");
+var connection = mysql.createConnection({ 
+  port: 3007,
+  host: "localhost",
+  user: "root",
+  //password: " ",
+  database: "burgers_db"
+
+})
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
+});
+
+module.exports = connection;
