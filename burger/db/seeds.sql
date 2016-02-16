33707e90d8ebc45c67960a66a5a0cecd2f6a160e
Marcode777 <marc102202@hotmail.com>
2016-02-16 15:30:53 -0500

    added numerous types of burgers to database

--- burger/db/seeds.sql
+++ burger/db/seeds.sql
@@ -1,2 +1,8 @@
 INSERT INTO burgers 
-  (id, burger_name, devoured ) VALUES (1, "smashburger", false );
+  (id, burger_name, devoured ) VALUES (3, "fivefatguys", false );
+INSERT INTO burgers
+  (id, burger_name, devoured ) VALUES (4, "burgerkingroyal", false );
+INSERT INTO burgers
+  (id, burger_name, devoured ) VALUES (5, "shakingshack", false );
+
+  --this seeds.sql file, when prompted in mysql with the file path, e.g. mysql source /Users/marcosebello/Desktop/RCB/eatdaburger/burger/db/seeds.sql, will automatically populate table in mqsl
