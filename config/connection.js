318c448b9a32800831f65769a9e2d07a4165af87
Marcode777 <marcosebello7@gmail.com>
2016-04-26 21:08:36 -0400

    modified connection.js

--- config/connection.js
+++ config/connection.js
@@ -11,14 +11,16 @@ app.engine("handlebars", exphbs({defaultLayout:"main"}));
 app.set("view engine", "handlebars");
 
 var mysql = require("mysql");
-var connection = mysql.createConnection({ 
-  port: 3306, //btw port 3306 is common convention for a mysql port; this mysql port has to be a different port from the node port 3306 is the default mysql port
-  host: "z12itfj4c1vgopf8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", // this was "localhost" before utilizing JAWSDB in order to successfully connect to heroku
-  user: "mzi7vmp5g0boo6o9", // this was "root" before utilizing mzi7vmp5g0boo6o9 to connect to JAWSDB for heroku
-  password: "unsve3rgn63zlu7f", // this password part was commented out before utilizing JAWSDB to connect to heroku
-  database: "burgers_db" // this is also the database name for heroku
+// var connection = mysql.createConnection({ 
+//   port: 3306, //btw port 3306 is common convention for a mysql port; this mysql port has to be a different port from the node port 3306 is the default mysql port
+//   host: "z12itfj4c1vgopf8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", // this was "localhost" before utilizing JAWSDB in order to successfully connect to heroku
+//   user: "mzi7vmp5g0boo6o9", // this was "root" before utilizing mzi7vmp5g0boo6o9 to connect to JAWSDB for heroku
+//   password: "unsve3rgn63zlu7f", // this password part was commented out before utilizing JAWSDB to connect to heroku
+//   database: "burgers_db" // this is also the database name for heroku
 
-})
+// })
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
+
 
 connection.connect(function(err) {
     if (err) {
